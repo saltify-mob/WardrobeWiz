e3876ec59a025faa054855a99f62fb5ced42a048
Grim <grim.e.kalman@gmail.com>
2023-11-20 15:57:56 +0100

    Add non-null assertion to the enviroment variables

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
 }) {
   return (
     <Auth0Provider
-      domain={process.env.AUTH0_DOMAIN}
-      clientId={process.env.AUTH0_CLIENT_ID}
+      domain={process.env.AUTH0_DOMAIN!}
+      clientId={process.env.AUTH0_CLIENT_ID!}
       authorizationParams={{
         redirect_uri: window.location.origin
       }}
