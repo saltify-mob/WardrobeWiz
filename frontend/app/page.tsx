74332dea0d30af11c6f438c9ee62aedf62e3b7be
Grim <grim.e.kalman@gmail.com>
2023-11-20 18:58:08 +0100

    Add authentication check to the Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,14 +3,21 @@
 import LoginForm from "./components/loginForm/LoginForm";
 import GeolocationComponent from "./components/WeatherCardComponent";
 import BottomNavBar from "./components/navBarComponent/NavBarComponent";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Home() {
+  const { isAuthenticated } = useAuth0();
+
 
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <LoginForm />
-        <GeolocationComponent />
-        <BottomNavBar />
+          {!isAuthenticated && <LoginForm />}
+          {isAuthenticated && (
+          <>
+            <GeolocationComponent />
+            <BottomNavBar />
+          </>
+          )}
       </main>
   );
 }
