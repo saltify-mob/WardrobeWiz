4722de7f02c1f2e0f72ec6742170b01f7c680de5
Grim <grim.e.kalman@gmail.com>
2023-11-20 20:17:10 +0100

    Refactor callback page

--- frontend/app/logincallback/page.tsx
+++ frontend/app/logincallback/page.tsx
@@ -2,19 +2,27 @@
 
 import { useAuth0 } from "@auth0/auth0-react";
 import { useRouter } from 'next/navigation';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const LoginCallback = () => {
   const { handleRedirectCallback, isAuthenticated } = useAuth0();
   const router = useRouter();
+  const [authHandled, setAuthHandled] = useState(false);
 
   useEffect(() => {
     const handleAuth0Callback = async () => {
       await handleRedirectCallback();
-      router.push(isAuthenticated ? '/' : '/login');
+      setAuthHandled(true); // Indicate that Auth0 callback has been handled
     };
     handleAuth0Callback();
-  }, [handleRedirectCallback, isAuthenticated, router]);
+  }, [handleRedirectCallback, router]);
+
+  useEffect(() => {
+    if (authHandled) {
+      // Redirect based on updated isAuthenticated state
+      router.push(isAuthenticated ? '/' : '/login');
+    }
+  }, [authHandled, isAuthenticated, router]);
 
   return <div>Loading...</div>;
 };
