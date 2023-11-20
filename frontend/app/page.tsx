074f3ba17999a48c4335ec8a3a4d25373edd9996
Grim <grim.e.kalman@gmail.com>
2023-11-20 18:42:29 +0100

    Add consol log to debug the env. variables

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ import BottomNavBar from "./components/navBarComponent/NavBarComponent";
 
 export default function Home() {
 
-  console.log(process.env.REACT_APP_AUTH0_DOMAIN)
+  console.log(process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID)
 
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
