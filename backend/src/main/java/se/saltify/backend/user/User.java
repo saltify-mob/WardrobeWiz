ff90c5cfff9aa03b6e0b7e53d8803349e15ea48c
AnneMariel95 <anne-mariel.catapang@appliedtechnology.se>
2023-11-20 09:19:26 +0100

    create user entity

---
+++ backend/src/main/java/se/saltify/backend/user/User.java
@@ -0,0 +1,47 @@
+package se.saltify.backend.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    public User() {
+    }
+
+    public User(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Id
+    @Column
+    private String id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
