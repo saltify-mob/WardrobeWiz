88b7e9c8f8165f4999dd72f879cb1917109bdb36
Grim <grim.e.kalman@gmail.com>
2023-11-20 18:53:40 +0100

    Refactor LoginForm

--- frontend/app/components/loginForm/LoginForm.tsx
+++ frontend/app/components/loginForm/LoginForm.tsx
@@ -1,17 +1,21 @@
 const LoginForm = () => {
-  const buildAuthUrl = (connection: string) => {
+  const handleLogin = (connection: string) => {
     const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
     const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
     const redirectUri = encodeURIComponent('https://wardrobe-wiz.vercel.app/');
     const responseType = 'code';
-  
-    return `https://${domain}/authorize?response_type=${responseType}&client_id=${clientId}&connection=${connection}&redirect_uri=${redirectUri}`;
+    const authUrl = `https://${domain}/authorize?response_type=${responseType}&client_id=${clientId}&connection=${connection}&redirect_uri=${redirectUri}`;
+    window.location.href = authUrl;
   };
 
   return (
     <div>
-      <a href={buildAuthUrl('google-oauth2')} className="login-button">Login with Google</a>
-      <a href={buildAuthUrl('facebook')} className="login-button">Login with Facebook</a>
+      <button onClick={() => handleLogin('google-oauth2')} className="login-button">
+        Login with Google
+      </button>
+      <button onClick={() => handleLogin('facebook')} className="login-button">
+        Login with Facebook
+      </button>
     </div>
   );
 };
