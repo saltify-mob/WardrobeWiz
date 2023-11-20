56905ac4cc9e42e38d61b4fed176ec470847cc5c
Grim <grim.e.kalman@gmail.com>
2023-11-20 19:37:06 +0100

    Add redirect to login page if not authenticated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,28 +1,25 @@
 'use client';
 
-import LoginForm from "./components/loginForm/LoginForm";
-import GeolocationComponent from "./components/WeatherCardComponent";
-import BottomNavBar from "./components/navBarComponent/NavBarComponent";
-import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth0 } from '@auth0/auth0-react';
+import GeolocationComponent from './components/WeatherCardComponent';
+import BottomNavBar from './components/navBarComponent/NavBarComponent';
 
 export default function Home() {
   const { isAuthenticated } = useAuth0();
+  const router = useRouter();
 
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push('/login');
+    }
+  }, [isAuthenticated, router]);
 
-  return (
-    <Auth0Provider
-      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
-      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
-    >
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          {!isAuthenticated && <LoginForm />}
-          {isAuthenticated && (
-          <>
-            <GeolocationComponent />
-            <BottomNavBar />
-          </>
-          )}
-      </main>
-    </Auth0Provider>
+  return isAuthenticated && (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <GeolocationComponent />
+      <BottomNavBar />
+    </main>
   );
 }
