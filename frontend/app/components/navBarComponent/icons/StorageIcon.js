24cd97784194bbd81f267343fdc0ebd72fa026f8
samimu7 <samikhishali@gmail.com>
2023-11-20 16:56:01 +0100

    add: crud navbar

---
+++ frontend/app/components/navBarComponent/icons/StorageIcon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const StorageIcon = () => (
+    <svg width="26" height="32" viewBox="0 0 26 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <line x1="10.0135" y1="12.973" x2="10.0135" y2="17.2973" stroke="#E6F4F1"/>
+    <line x1="10.0135" y1="12.973" x2="10.0135" y2="17.2973" stroke="#E6F4F1"/>
+    <line x1="10.0135" y1="12.973" x2="10.0135" y2="17.2973" stroke="#E6F4F1"/>
+    <rect x="0.75" y="0.75" width="23.5811" height="28.7703" rx="3.25" stroke="#E6F4F1" stroke-width="1.5"/>
+    <line x1="12.6081" x2="12.6081" y2="30.2703" stroke="#E6F4F1"/>
+    <line x1="15.2027" y1="12.973" x2="15.2027" y2="17.2973" stroke="#E6F4F1"/>
+    <line x1="15.2027" y1="12.973" x2="15.2027" y2="17.2973" stroke="#E6F4F1"/>
+    <line x1="15.2027" y1="12.973" x2="15.2027" y2="17.2973" stroke="#E6F4F1"/>
+    <line x1="6.80408" y1="30.2703" x2="6.80408" y2="32" stroke="#E6F4F1" stroke-width="1.5"/>
+    <line x1="18.0473" y1="30.2703" x2="18.0473" y2="32" stroke="#E6F4F1" stroke-width="1.5"/>
+    </svg>
+    
+);
+
+export default StorageIcon;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,10 +2,15 @@
 
 import { Auth0Provider } from "@auth0/auth0-react";
 import LoginForm from "./components/loginForm/LoginForm";
+import GeolocationComponent from "./components/WeatherCardComponent";
+import BottomNavBar from "./components/navBarComponent/NavBarComponent";
+
 
 export default function Home() {
 
   return (
+   
+    
     <Auth0Provider
       domain={process.env.AUTH0_DOMAIN!}
       clientId={process.env.AUTH0_CLIENT_ID!}
@@ -15,7 +20,10 @@ export default function Home() {
     >
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <LoginForm />
+        <GeolocationComponent />
+    <BottomNavBar />
       </main>
     </Auth0Provider>
+    
   );
 }
