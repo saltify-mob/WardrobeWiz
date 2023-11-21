82204c5e2991d67b31bb29f952f34684ebc0779a
Grim <grim.e.kalman@gmail.com>
2023-11-21 09:17:35 +0100

    Refactor following the setup doc from Auth0

--- frontend/app/callback/page.tsx
+++ frontend/app/callback/page.tsx
@@ -1,28 +1,7 @@
 "use client"
 
-import { useAuth0 } from "@auth0/auth0-react";
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from "react";
-
 const Callback = () => {
-  const { handleRedirectCallback, isAuthenticated } = useAuth0();
-  const router = useRouter();
-  const [authHandled, setAuthHandled] = useState(false);
-
-  useEffect(() => {
-    const handleAuth0Callback = async () => {
-      await handleRedirectCallback();
-      setAuthHandled(true);
-    };
-    handleAuth0Callback();
-  }, [handleRedirectCallback, router]);
-
-  useEffect(() => {
-    if (authHandled) {
-      router.push(isAuthenticated ? '/' : '/login');
-    }
-  }, [authHandled, isAuthenticated, router]);
-
+  
   return "Hello"
 };
 
