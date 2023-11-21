e833c01ba349e2f3e795d02cc111865b2b05a6ae
Grim <grim.e.kalman@gmail.com>
2023-11-21 18:15:21 +0100

    Removed isAuthenticated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,19 +3,16 @@
 import GeolocationComponent from './components/weatherCard/WeatherCard';
 import BottomNavBar from './components/navBar/NavBar';
 import TodaysOutfit from './components/suggestionCard/SuggestionCard';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import Login from './login';
 
 export default function Home() {
-  const { isAuthenticated } = useAuth0();
    
-  return isAuthenticated && (
+  return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       {/* <GeolocationComponent /> */}
-      <TodaysOutfit/>
+      <Login />
+      <TodaysOutfit />
       <BottomNavBar />
-      <h1>whaaat??</h1>
     </main>
   );
 }
