26c37690af5275ceacd29d5a2c1ed5f4b57b74ef
Grim <grim.e.kalman@gmail.com>
2023-11-21 13:27:57 +0100

    Testing getting the user info

--- frontend/app/callback/page.tsx
+++ frontend/app/callback/page.tsx
@@ -3,13 +3,32 @@
 import { useEffect, useState } from 'react';
 
 const Callback = () => {
-  const [hash, setHash] = useState('');
+  const [userInfo, setUserInfo] = useState(null);
 
   useEffect(() => {
-    setHash(window.location.hash);
+    const hash = window.location.hash;
+    const params = new URLSearchParams(hash.substring(1)); // Remove the '#' and parse
+    const accessToken = params.get('access_token');
+
+    if (accessToken) {
+      fetch('https://dev-zhqvuxtn6agalo22.us.auth0.com/userinfo', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        setUserInfo(data);
+      })
+      .catch(error => {
+        console.error('Error fetching user info:', error);
+      });
+    }
   }, []);
 
-  return <div>{hash}</div>;
+  if (!userInfo) return <div>Loading user info...</div>;
+
+  return <div>{JSON.stringify(userInfo)}</div>;
 };
 
 export default Callback;
