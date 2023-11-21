5ac1e9e4f329281b7233477e1d9791810158fdea
Grim <grim.e.kalman@gmail.com>
2023-11-21 13:46:02 +0100

    Add reroute to the callback

--- frontend/app/callback/page.tsx
+++ frontend/app/callback/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 const Callback = () => {
   const [userInfo, setUserInfo] = useState(null);
+  const router = useRouter();
+
 
   useEffect(() => {
     const hash = window.location.hash;
@@ -19,12 +22,16 @@ const Callback = () => {
       .then(response => response.json())
       .then(data => {
         setUserInfo(data);
+        router.push('/');
       })
       .catch(error => {
         console.error('Error fetching user info:', error);
+        router.push('/login');
       });
+    } else {
+      router.push('/login');
     }
-  }, []);
+  }, [router]);
 
   if (!userInfo) return <div>Loading user info...</div>;
 
