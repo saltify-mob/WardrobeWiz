b4cdf57140cf23aebcc493e0748758cb6098e538
Grim <grim.e.kalman@gmail.com>
2023-11-21 09:27:06 +0100

    Refactor redid the change with auth0 basic setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth0 } from '@auth0/auth0-react';
 import GeolocationComponent from './components/weatherCard/WeatherCard';
 import BottomNavBar from './components/navBar/NavBar';
 
 export default function Home() {
+  const { isAuthenticated } = useAuth0();
+  const router = useRouter();
 
-  return (
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push('/login');
+    }
+  }, [isAuthenticated, router]);
+
+  return isAuthenticated && (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <GeolocationComponent />
       <BottomNavBar />
