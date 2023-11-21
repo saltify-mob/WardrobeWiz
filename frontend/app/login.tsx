0b43033f76ed3eef5856e05a5c09c3c99561b501
Grim <grim.e.kalman@gmail.com>
2023-11-21 18:32:58 +0100

    debug

--- frontend/app/login.tsx
+++ frontend/app/login.tsx
@@ -1,3 +1,3 @@
 export default function Login() {
-    return <a href="dev-zhqvuxtn6agalo22.us.auth0.com/login">Login</a>;
+    return <a href="https://dev-zhqvuxtn6agalo22.us.auth0.com/login">Login</a>;
   }
