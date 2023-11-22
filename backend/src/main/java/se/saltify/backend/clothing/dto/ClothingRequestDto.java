137b425a51e4945da77f8a29dd2b13a0e953d1da
AnneMariel95 <anne-mariel.catapang@appliedtechnology.se>
2023-11-22 09:35:22 +0100

    add category

--- backend/src/main/java/se/saltify/backend/clothing/dto/ClothingRequestDto.java
+++ backend/src/main/java/se/saltify/backend/clothing/dto/ClothingRequestDto.java
@@ -1,4 +1,4 @@
 package se.saltify.backend.clothing.dto;
 
-public record ClothingRequestDto(String userId, String type, String season, String color, String dateOfPurchase, String timeLastUsed) {
+public record ClothingRequestDto(String userId, String category, String type, String season, String color, String dateOfPurchase, String timeLastUsed) {
 }
