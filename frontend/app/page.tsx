ca569e25c1df7b26c866efd8bdf1fd697f409d00
Grim <grim.e.kalman@gmail.com>
2023-11-22 13:42:36 +0100

    Add router

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,11 +4,13 @@ import GeolocationComponent from './components/weatherCard/WeatherCard';
 import BottomNavBar from './components/navBar/NavBar';
 import TodaysOutfit from './components/suggestionCard/SuggestionCard';
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
   const router = useRouter();
+
+  console.log(user)
 
   if (user) {
     return (
