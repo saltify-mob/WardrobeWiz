867b9cf9d0a9f586bf8d51e374005684536ae6c9
Grim <grim.e.kalman@gmail.com>
2023-11-22 14:20:25 +0100

    Add handleLogin

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,23 +5,20 @@ import BottomNavBar from './components/navBar/NavBar';
 import TodaysOutfit from './components/suggestionCard/SuggestionCard';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { handleLogin } from '@auth0/nextjs-auth0';
+import Link from 'next/link';
 
 export default function Home() {
   const { user } = useUser();
 
   if (!user) {
-    handleLogin();
+    console.log("hello")
   }
 
-  if (user) {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-4">
-        <GeolocationComponent />
-        <TodaysOutfit />
-        <BottomNavBar />
-      </main>
-    );
-  }
-
-  return "";
+  return (user) && (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <GeolocationComponent />
+      <TodaysOutfit />
+      <BottomNavBar />
+    </main>
+  );
 }
