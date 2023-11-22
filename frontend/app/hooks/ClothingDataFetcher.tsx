3fdf7957c5b297525e2e538c39d5b80f83bdb17d
samimu7 <samikhishali@gmail.com>
2023-11-22 15:37:18 +0100

    add: fetchedallclothes are store in localstorage for user, set fetchallclothes to run when you enter the infopage

--- frontend/app/hooks/ClothingDataFetcher.tsx
+++ frontend/app/hooks/ClothingDataFetcher.tsx
@@ -1,23 +1,26 @@
 type ClothingItem = {
-    id: string;
-    type: string;
-    season: string;
-    color: string;
-    dateOfPurchase: string;
-    timeLastUsed: string;
-    imageUrl: string;
-  };
-  
-  export const fetchClothingData = async () => {
-    try {
-      const response = await fetch('https://wardrobewiz-backend.azurewebsites.net/api/clothings');
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data: ClothingItem[] = await response.json();
-      console.log('Fetched Clothing Data:', data);
-    } catch (error) {
-      console.error('Error fetching clothing data:', error);
+  id: string;
+  type: string;
+  season: string;
+  color: string;
+  dateOfPurchase: string;
+  timeLastUsed: string;
+  imageUrl: string;
+};
+
+export const fetchClothingData = async () => {
+  try {
+    const response = await fetch('https://wardrobewiz-backend.azurewebsites.net/api/clothings');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-  };
-  
+    const data: ClothingItem[] = await response.json();
+    
+    // Store the data in local storage
+    localStorage.setItem('clothingData', JSON.stringify(data));
+
+    console.log('Fetched Clothing Data:', data);
+  } catch (error) {
+    console.error('Error fetching clothing data:', error);
+  }
+};
--- frontend/app/tabs/info/page.tsx
+++ frontend/app/tabs/info/page.tsx
@@ -1,14 +1,16 @@
-'use client';
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import BottomNavBar from '../../components/navBar/NavBar';
-
+import { fetchClothingData } from '../../hooks/ClothingDataFetcher'; // Adjust the path accordingly
 
 export default function Info() {
+  useEffect(() => {
+    fetchClothingData();
+  }, []); // The empty array ensures this runs only once when the component mounts
+
   return (
-  <main className="flex min-h-screen flex-col items-center justify-between p-4">
-  <div>Info Page</div>
-  <BottomNavBar />
-</main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <div>Info Page</div>
+      <BottomNavBar />
+    </main>
   );
 }
