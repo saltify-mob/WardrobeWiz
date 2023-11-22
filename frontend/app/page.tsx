b803e827302d2a5ebcbc52fe6bc524b90746935a
Grim <grim.e.kalman@gmail.com>
2023-11-22 11:49:41 +0100

    Add authentication

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,14 +3,20 @@
 import GeolocationComponent from './components/weatherCard/WeatherCard';
 import BottomNavBar from './components/navBar/NavBar';
 import TodaysOutfit from './components/suggestionCard/SuggestionCard';
-import Login from './login';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-   
-  return (
+  const { isAuthenticated } = useAuth0();
+  const router = useRouter();
+
+  if (!isAuthenticated) {
+    router.push("/api/auth/login");
+  }
+
+  return isAuthenticated && (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <GeolocationComponent />
-      <Login />
       <TodaysOutfit />
       <BottomNavBar />
     </main>
