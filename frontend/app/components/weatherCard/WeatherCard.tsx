425e8aa0eff7b116000615867f1c0e80777d76ee
Grim <grim.e.kalman@gmail.com>
2023-11-22 19:12:17 +0100

    Add on success to useEffect

--- frontend/app/components/weatherCard/WeatherCard.tsx
+++ frontend/app/components/weatherCard/WeatherCard.tsx
@@ -87,7 +87,7 @@ const GeolocationComponent: React.FC = () => {
     return () => {
       navigator.geolocation.clearWatch(watcher);
     };
-  }, []);
+  }, [onSuccess]);
 
   const WeatherIcon = ({ code }: { code: string }) => {
     const iconUrl = `http://openweathermap.org/img/wn/${code}.png`;
@@ -96,8 +96,6 @@ const GeolocationComponent: React.FC = () => {
         <Image
           src={iconUrl}
           alt="Weather icon"
-          width={50}
-          height={50}
           layout="fixed"
         />
       </div>
