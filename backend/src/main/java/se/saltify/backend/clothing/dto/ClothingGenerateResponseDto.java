99a2371a0a92b63f66fbae28bb5e3154d619794f
AnneMariel95 <anne-mariel.catapang@appliedtechnology.se>
2023-11-22 13:54:12 +0100

    remove id

--- backend/src/main/java/se/saltify/backend/clothing/dto/ClothingGenerateResponseDto.java
+++ backend/src/main/java/se/saltify/backend/clothing/dto/ClothingGenerateResponseDto.java
@@ -2,5 +2,5 @@ package se.saltify.backend.clothing.dto;
 
 import se.saltify.backend.clothing.Clothing;
 
-public record ClothingGenerateResponseDto(String id, Clothing headwear, Clothing top, Clothing lowerGarment) {
+public record ClothingGenerateResponseDto(Clothing headwear, Clothing top, Clothing lowerGarment) {
 }
