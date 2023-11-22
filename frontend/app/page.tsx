36f02cc510d25646bdb427e615dd85ce679789ab
Grim <grim.e.kalman@gmail.com>
2023-11-22 13:31:36 +0100

    Remove login button and replaced with redirect

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,6 +18,8 @@ export default function Home() {
       </main>
     );
   } else {
-    window.location.href = "/api/auth/login";
+    if (typeof window !== "undefined") {
+      window.location.href = "/api/auth/login";
+    }    
   }
 }
