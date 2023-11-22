f7a4e9f3c4862f39b9d61a15623b74a40ab52182
Grim <grim.e.kalman@gmail.com>
2023-11-22 14:35:16 +0100

    Add useffect

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,20 +4,24 @@ import GeolocationComponent from './components/weatherCard/WeatherCard';
 import BottomNavBar from './components/navBar/NavBar';
 import TodaysOutfit from './components/suggestionCard/SuggestionCard';
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { handleLogin } from '@auth0/nextjs-auth0';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
   const { user } = useUser();
+  const router = useRouter();
 
-  if (!user) {
-    return <div>Hello!</div>
-  } else {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-4">
-        <GeolocationComponent />
-        <TodaysOutfit />
-        <BottomNavBar />
-      </main>
-    );
-  }
+  useEffect(() => {
+    if (!user) {
+      router.push('/api/auth/login');
+    }
+  }, [user, router]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <GeolocationComponent />
+      <TodaysOutfit />
+      <BottomNavBar />
+    </main>
+  );
 }
