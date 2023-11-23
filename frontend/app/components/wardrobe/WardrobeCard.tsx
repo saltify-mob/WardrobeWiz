122fde2c7e96a92b56dea83f8009af63a357039b
JacobLars <jacoblar2001@gmail.com>
2023-11-23 11:07:53 +0100

    add: wardrobe components

---
+++ frontend/app/components/wardrobe/WardrobeCard.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import HeadWearCard from './headwear/HeadWearCard';
+import TopsCard from './tops/TopsCard';
+
+interface Clothing {
+  id: string;
+  category: string;
+  type: string;
+  season: string;
+  color: string;
+  dateOfPurchase: string;
+  timeLastUsed: string;
+  imageUrl: string;
+}
+
+const WardrobeCard = () => {
+
+  const [wardrobe, setWardrobe] = useState<Clothing[]>([]);
+
+  useEffect(() => {
+    const getWardrobe = async () => {
+    try {
+      const response = await fetch('https://wardrobewiz-backend.azurewebsites.net/api/clothings/wardrobe/0bde3294-0feb-41e0-8763-4c6477623f5e');
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setWardrobe(data);
+    } catch (error) {
+      console.error('Error fetching clothing data:', error);
+    }
+  };
+  getWardrobe(); 
+  }, []);
+  const headWears = wardrobe.filter(cloting => cloting.category === 'headwear');
+  const tops = wardrobe.filter(cloting => cloting.category === 'top');
+  const lowerGarments = wardrobe.filter(cloting => cloting.category === 'lowerGarment');
+  return (
+    <div className='flex flex-col'>
+        <HeadWearCard headwears={headWears} />
+        <TopsCard tops={tops} />
+    </div>
+  )
+}
+
+export default WardrobeCard;
---
+++ frontend/app/components/wardrobe/headwear/HeadWearCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+interface Clothing {
+    id: string;
+    category: string;
+    type: string;
+    season: string;
+    color: string;
+    dateOfPurchase: string;
+    timeLastUsed: string;
+    imageUrl: string;
+  }
+interface Props{
+    headwears: Clothing[]
+}
+
+const HeadWearCard = (props: Props) => {
+
+  return (
+    <div className='flex flex-row'>
+        {props.headwears.map((h, index) => (
+            <img height={100} width={200} key={index} src={h.imageUrl}></img>
+        ))}
+    </div>
+  )
+}
+
+export default HeadWearCard;
