d88b1472775243e6c0e2a9f81645b8a29c58e124
AnneMariel95 <anne-mariel.catapang@appliedtechnology.se>
2023-11-23 10:19:14 +0100

    fetch clothings

--- frontend/app/clothings/page.tsx
+++ frontend/app/clothings/page.tsx
@@ -1,4 +1,7 @@
 'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 export const HOST_NAME = 'https://wardrobewiz-backend.azurewebsites.net';
 
@@ -12,10 +15,34 @@ export type Clothes = {
 };
 
 export default function ClothingsPage() {
+  const [clothes, setClothes] = useState<Clothes[]>([]);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch(`${HOST_NAME}/api/clothings`)
+      .then((res) => res.json())
+      .then((res) => setClothes(res));
+  }, []);
+
   return (
     <div>
-      {' '}
-      <main></main>{' '}
+      <div className="flex flex-col gap-2">
+        {clothes.map((cloth) => (
+          <div key={cloth.id}>
+            <p>Type: {cloth.type}</p>
+            <p>Category: {cloth.category}</p>
+            <p>Season: {cloth.season}</p>
+            <p>Color: {cloth.color}</p>
+            <Image
+              src={cloth.imageUrl}
+              alt={cloth.id}
+              height={300}
+              width={300}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
