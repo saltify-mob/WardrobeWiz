9a3c859edf041e444036961df82795ecd4a58049
samimu7 <samikhishali@gmail.com>
2023-11-23 16:43:25 +0100

    add: storagepage.tsx

--- frontend/app/tabs/storage/page.tsx
+++ frontend/app/tabs/storage/page.tsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import BottomNavBar from '../../components/navBar/NavBar';
+import StorageCard from '../../components/storage/StorageCard';
 
 
 export default function Storage() {
   return (
   <main className="flex min-h-screen flex-col items-center justify-between p-4">
   <div>Storage Page</div>
+  <StorageCard/>
   <BottomNavBar />
 </main>
   );
