8f1fcec9b538059c2712aaa01aaf116a298023e5
AnneMariel95 <anne-mariel.catapang@appliedtechnology.se>
2023-11-23 17:50:13 +0100

    use fetcher in listing clothings

--- frontend/app/hooks/ClothingDataFetcher.tsx
+++ frontend/app/hooks/ClothingDataFetcher.tsx
@@ -1,6 +1,8 @@
+import { fetcher } from '../utils/fetcher';
+
 export const fetchClothingData = async () => {
   try {
-    const response = await fetch('https://wardrobewiz-backend.azurewebsites.net/api/clothings');
+    const response = await fetcher('/api/clothings');
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
