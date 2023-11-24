44597d2b7efced398230faec49e48b958287b7ee
Grim <grim.e.kalman@gmail.com>
2023-11-24 18:14:31 +0100

    Add createUser hook

--- frontend/app/hooks/createUser.tsx
+++ frontend/app/hooks/createUser.tsx
@@ -2,10 +2,11 @@ export const createUser = async (user: string) => {
     const response = await fetch("https://wardrobewiz-backend.azurewebsites.net/api/users/" + user);
     if (!response.ok) {
         const data = '{' +
-                    '"id":' + {user} + ',' +
+                    `"id":"${user}",` +
                     '"firstName":"Grim",' +
                     '"lastName":"Was here!"'
                     +'}';
+
         const response = await fetch("https://wardrobewiz-backend.azurewebsites.net/api/users/", {
             method: "POST",
             headers: {
@@ -13,7 +14,8 @@ export const createUser = async (user: string) => {
             },
             body: JSON.stringify(data)
           });
-
-        console.log(response);
+        
+          
+        console.log(response.json.toString)
     }
 };  
