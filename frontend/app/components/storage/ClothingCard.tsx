e4027abf23ad1b9032fcb3b6113545ba630e7de2
Grim <grim.e.kalman@gmail.com>
2023-11-25 10:49:53 +0100

    Refactor ClothingCard to use Image instead of img

--- frontend/app/components/storage/ClothingCard.tsx
+++ frontend/app/components/storage/ClothingCard.tsx
@@ -13,7 +13,14 @@ const ClothingCard: React.FC<ClothingDetailProps> = ({ clothing, onClose, onDele
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-90">
       <div className="max-w-md p-4 bg-white shadow-md rounded-md">
-        <img src={clothing.imageUrl} alt={clothing.type} className="w-full h-auto" />
+        <Image 
+          src={clothing.imageUrl} 
+          alt={clothing.type} 
+          layout="responsive"
+          width={170}
+          height={227}
+          objectFit="cover"
+        />
         <div className="mt-4">
           <button onClick={onDelete} className="mr-2">Delete</button>
           <button onClick={onSendToWardrobe}>Send to Wardrobe</button>
