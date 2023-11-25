79c99e906de526d4fe849ea1228f6d35dde897d2
Grim <grim.e.kalman@gmail.com>
2023-11-25 13:07:18 +0100

    Add logout button

--- frontend/app/settings/page.tsx
+++ frontend/app/settings/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import HamburgerMenu from '../components/hamburgerMenu/HamburgerMenu';
 
 export default function Settings() {
@@ -8,6 +9,7 @@ export default function Settings() {
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <HamburgerMenu />
       <div>Settings Page</div>
+      <Link href="/api/auth/logout">Logout</Link>
     </main>
   );
 }
