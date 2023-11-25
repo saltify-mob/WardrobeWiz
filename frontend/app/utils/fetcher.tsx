b5871396b0f296053aa5d1e2b1671df88f71449a
Grim <grim.e.kalman@gmail.com>
2023-11-25 11:54:34 +0100

    Refactor fetcher to auto include application/JSON or FormData depending on which

--- frontend/app/utils/fetcher.tsx
+++ frontend/app/utils/fetcher.tsx
@@ -4,11 +4,13 @@ export const HOST_NAME = 'https://wardrobewiz-backend.azurewebsites.net';
 export const fetcher = (url: string, init?: RequestInit) => {
   const userId = localStorage.getItem('user_id') as string;
 
-  const headers = {
-    'Content-Type': 'application/json',
-    'ww-user-id': userId,
-    ...init?.headers,
-  };
+  const headers: HeadersInit = new Headers(init?.headers);
+  headers.set('ww-user-id', userId);
+
+  // Add 'Content-Type': 'application/json' only if body exists and it's not FormData
+  if (init?.body && !(init.body instanceof FormData)) {
+    headers.set('Content-Type', 'application/json');
+  }
 
   return fetch(HOST_NAME + url, {
     ...init,
