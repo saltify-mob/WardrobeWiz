b2fd5714af9bbd852d690af28f9bee0f82a7ff57
Grim <grim.e.kalman@gmail.com>
2023-11-25 12:51:31 +0100

    Refactor wardrobe

--- frontend/app/wardrobe/page.tsx
+++ frontend/app/wardrobe/page.tsx
@@ -8,10 +8,9 @@ import HamburgerMenu from '../components/hamburgerMenu/HamburgerMenu';
 export default function Wardrobe() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      <h1 className='text-2xl font-bold'>My Wardrobe</h1>
       <HamburgerMenu />
+      <h1 className='text-2xl font-bold'>My Wardrobe</h1>
       <WardrobeCard />
-      <BottomNavBar />
     </main>
   );
 }
