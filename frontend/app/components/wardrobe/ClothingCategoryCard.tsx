742512da6241f0fdb50b86402b37c98d50baceb0
AnneMariel95 <anne-mariel.catapang@appliedtechnology.se>
2023-11-27 22:38:04 +0100

    fix wardrobe carousel issue

--- frontend/app/components/wardrobe/ClothingCategoryCard.tsx
+++ frontend/app/components/wardrobe/ClothingCategoryCard.tsx
@@ -2,7 +2,6 @@ import React, { useRef } from 'react';
 import Image from 'next/image';
 import { ClothingItem } from '@/app/types/ClothingItem';
 import { Carousel, IconButton } from '@material-tailwind/react';
-import Link from 'next/link';
 
 interface Props {
   clothingItems: ClothingItem[];
@@ -16,7 +15,7 @@ const ClothingCategoryCard = ({ clothingItems, onToggleDetail, categoryTitle }: 
     <div className='flex flex-col items-center justify-center'>
       <h2 className="text-xl font-semibold mb-4 my-4">{categoryTitle}</h2>
       <Carousel
-        className="w-full md:w-1/3 h-64"
+        className="w-full md:w-1/3 h-72"
         navigation={({ setActiveIndex, activeIndex, length }) => (
           <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
             {new Array(length).fill("").map((_, i) => (
@@ -29,70 +28,74 @@ const ClothingCategoryCard = ({ clothingItems, onToggleDetail, categoryTitle }: 
               />
             ))}
           </div>
-    )}
-    prevArrow={({ handlePrev }) => (
-      <IconButton
-        variant="text"
-        color="white"
-        size="lg"
-        onClick={handlePrev}
-        className="!absolute top-2/4 left-4 -translate-y-2/4 bg-base-300"
+        )}
+        prevArrow={({ handlePrev }) => (
+          <IconButton
+            variant="text"
+            color="white"
+            size="lg"
+            onClick={handlePrev}
+            className="!absolute top-2/4 left-4 -translate-y-2/4 bg-base-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+              />
+            </svg>
+          </IconButton>
+        )}
+        nextArrow={({ handleNext }) => (
+          <IconButton
+            variant="text"
+            color="white"
+            size="lg"
+            onClick={handleNext}
+            className="!absolute top-2/4 right-4 -translate-y-2/4 bg-base-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </IconButton>
+        )}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-          />
-        </svg>
-      </IconButton>
-    )}
-    nextArrow={({ handleNext }) => (
-      <IconButton
-        variant="text"
-        color="white"
-        size="lg"
-        onClick={handleNext}
-        className="!absolute top-2/4 right-4 -translate-y-2/4 bg-base-300"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-          />
-        </svg>
-      </IconButton>
-      )}>
-      {clothingItems.map((item, index) => (
-        <div key={index} onClick={() => onToggleDetail(item)}>
-           <Image
-           key={index}
-           layout="fill"
-           objectFit="cover"
-           src={item.imageUrl}
-           alt="image"
-           className="w-full"
-         />
-         </div>
-      ))} 
-    </Carousel>
+        {clothingItems.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => onToggleDetail(item)}
+            className="w-full h-72 flex justify-center"
+          >
+            <Image
+              onClick={() => onToggleDetail(item)}
+              src={item.imageUrl}
+              height={288}
+              width={288}
+              alt="image"
+            />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
-}
+};
 
 export default ClothingCategoryCard;
