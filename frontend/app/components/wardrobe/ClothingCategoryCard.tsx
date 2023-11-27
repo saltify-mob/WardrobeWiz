8358b2773b68fd42e14ee63d12bd501f272c799d
samimu7 <samikhishali@gmail.com>
2023-11-27 18:14:46 +0100

    style:wardrobe

--- frontend/app/components/wardrobe/ClothingCategoryCard.tsx
+++ frontend/app/components/wardrobe/ClothingCategoryCard.tsx
@@ -13,28 +13,30 @@ interface Props {
 const ClothingCategoryCard = ({ clothingItems, onToggleDetail, categoryTitle }: Props) => {
 
   return (
-    <div className='flex flex-row items-center justify-center'>
-    <Carousel className="w-1/3"
-    navigation={({ setActiveIndex, activeIndex, length }) => (
-      <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-        {new Array(length).fill("").map((_, i) => (
-          <span
-            key={i}
-            className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-              activeIndex === i ? "w-8 h-2 bg-black" : "w-4 bg-black"
-            }`}
-            onClick={() => setActiveIndex(i)}
-          />
-        ))}
-      </div>
+    <div className='flex flex-col items-center justify-center'>
+      <h2 className="text-xl font-semibold mb-4 my-4">{categoryTitle}</h2> {/* Add the title here */}
+      <Carousel
+        className="w-full md:w-1/3 h-64" // Set a square aspect ratio
+        navigation={({ setActiveIndex, activeIndex, length }) => (
+          <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+            {new Array(length).fill("").map((_, i) => (
+              <span
+                key={i}
+                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                  activeIndex === i ? "w-8 h-2 bg-primary" : "w-4 bg-base-300"
+                }`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
     )}
-     prevArrow={({ handlePrev }) => (
+    prevArrow={({ handlePrev }) => (
       <IconButton
         variant="text"
         color="white"
         size="lg"
         onClick={handlePrev}
-        className="!absolute top-2/4 left-4 -translate-y-2/4 bg-black"
+        className="!absolute top-2/4 left-4 -translate-y-2/4 bg-base-300"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -51,37 +53,37 @@ const ClothingCategoryCard = ({ clothingItems, onToggleDetail, categoryTitle }: 
           />
         </svg>
       </IconButton>
-       )}
-      nextArrow={({ handleNext }) => (
-        <IconButton
-          variant="text"
-          color="white"
-          size="lg"
-          onClick={handleNext}
-          className="!absolute top-2/4 right-4 -translate-y-2/4 bg-black"
+    )}
+    nextArrow={({ handleNext }) => (
+      <IconButton
+        variant="text"
+        color="white"
+        size="lg"
+        onClick={handleNext}
+        className="!absolute top-2/4 right-4 -translate-y-2/4 bg-base-300"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="h-6 w-6"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-            />
-          </svg>
-        </IconButton>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+          />
+        </svg>
+      </IconButton>
       )}>
       {clothingItems.map((item, index) => (
         <div key={index} onClick={() => onToggleDetail(item)}>
            <Image
            key={index}
-           width={1000}
-           height={1050}
+           layout="fill"
+           objectFit="cover" // Crop or zoom to fit
            src={item.imageUrl}
            alt="image"
            className="w-full"
