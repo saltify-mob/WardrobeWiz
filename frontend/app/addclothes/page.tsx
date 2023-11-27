ac45696d164e30b46619140192c2fda2ad1d8aef
samimu7 <samikhishali@gmail.com>
2023-11-27 11:33:08 +0100

    refactor: page.tsx for form to render new addclothes component

--- frontend/app/addclothes/page.tsx
+++ frontend/app/addclothes/page.tsx
@@ -1,136 +1,15 @@
-'use client';
-import { ChangeEvent, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import React from 'react';
 import HamburgerMenu from '../components/hamburgerMenu/HamburgerMenu';
-import { useWardrobe } from '../hooks/wardrobeContext';
+import AddClothingForm from '../components/addClothes/AddClothes'; // Adjust the import path as needed
 
-export default function AddClothingPage() {
-  const [file, setFile] = useState<Blob | null>(null);
-  const [season, setSeason] = useState('winter');
-  const [type, setType] = useState('shirt');
-  const [category, setCategory] = useState('top');
-  const [location, setLocation] = useState('wardrobe');
-  const [color, setColor] = useState('red');
-
-  // Placeholder:
-  const currentDate = new Date().toISOString().split('T')[0];
-  const [dateOfPurchase] = useState(currentDate);
-  const [timeLastUsed] = useState(currentDate);
-
-  const { handleAddClothing } = useWardrobe();
-  const router = useRouter();
-
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setFile(event.target.files[0]);
-    }
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.append('season', season);
-    formData.append('type', type);
-    formData.append('category', category);
-    formData.append('color', color);
-    formData.append('location', location);
-    formData.append('dateOfPurchase', dateOfPurchase);
-    formData.append('timeLastUsed', timeLastUsed);
-    if (file) {
-      formData.append('image', file);
-    }
-
-    await handleAddClothing(formData);
-    router.push('/wardrobe');
-  };
-
+const AddClothesPage: React.FC = () => {
   return (
-    <main className="w-full flex-col items-center justify-between">
+    <main className="flex flex-col items-center w-full">
       <HamburgerMenu />
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="season">Season: </label>
-          <select
-            id="season"
-            required
-            value={season}
-            placeholder="Enter season"
-            onChange={(e) => setSeason(e.target.value)}
-          >
-            <option value="winter">Winter</option>
-            <option value="spring">Spring</option>
-            <option value="summer">Summer</option>
-            <option value="autumn">Autumn</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="type">Type: </label>
-          <select
-            id="type"
-            required
-            value={type}
-            placeholder="Enter type"
-            onChange={(e) => setType(e.target.value)}
-          >
-            <option value="shirt">Shirt</option>
-            <option value="hoodie">Hoodie</option>
-            <option value="sweatShirt">Sweat Shirt</option>
-            <option value="t-shirt">T-shirt</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="category">Category: </label>
-          <select
-            id="category"
-            required
-            value={category}
-            placeholder="Enter category"
-            onChange={(e) => setCategory(e.target.value)}
-          >
-            <option value="headwear">Headwear</option>
-            <option value="top">Top</option>
-            <option value="lowerGarment">Lower Garment</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="location">Location: </label>
-          <select
-            id="location"
-            required
-            value={location}
-            placeholder="Enter location"
-            onChange={(e) => setLocation(e.target.value)}
-          >
-            <option value="wardrobe">Wardrobe</option>
-            <option value="storage">Storage</option>
-            <option value="luggage">Luggage</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="color">Color: </label>
-          <select
-            id="color"
-            required
-            value={color}
-            placeholder="Enter color"
-            onChange={(e) => setColor(e.target.value)}
-          >
-            <option value="blue">Blue</option>
-            <option value="red">Red</option>
-            <option value="yellow">Yellow</option>
-            <option value="green">Green</option>
-            <option value="orange">Orange</option>
-            <option value="pink">Pink</option>
-            <option value="purple">Purple</option>
-            <option value="brown">Brown</option>
-            <option value="black">Black</option>
-            <option value="white">White</option>
-          </select>
-        </div>
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Submit</button>
-      </form>
+      <h1 className="text-2xl font-bold my-4 text-center">Add Clothes</h1>
+      <AddClothingForm />
     </main>
   );
-}
+};
+
+export default AddClothesPage;
