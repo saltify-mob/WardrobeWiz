975400851f33e1cf45040411cfc772132a31cbc2
samimu7 <samikhishali@gmail.com>
2023-11-27 12:34:52 +0100

    add: styling to storage card

--- frontend/app/components/storage/StorageCard.tsx
+++ frontend/app/components/storage/StorageCard.tsx
@@ -40,41 +40,52 @@ const StorageCard: React.FC = () => {
   }
 
   const filteredClothes = wardrobe.filter((clothing) =>
-    clothing.location === "storage" && (
-    clothing.type.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    clothing.category.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    clothing.color.toLowerCase().includes(searchTerm.toLowerCase()))
+    clothing.location === 'storage' &&
+    (clothing.type.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      clothing.category.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      clothing.color.toLowerCase().includes(searchTerm.toLowerCase()))
   );
 
   return (
-    <div>
+    <div className="w-full max-w-screen-xl mx-auto px-4 py-6">
       <input
         type="text"
         placeholder="Search Clothes..."
         onChange={(e) => setSearchTerm(e.target.value)}
+        className="mb-4 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-3 py-2"
       />
-      <div>
+      <div className="grid grid-cols-3 gap-4">
         {filteredClothes.map((clothing, index) => (
-          <div key={index} className="relative cursor-pointer" onClick={() => toggleDetail(clothing)}>
-            <Image
-              src={clothing.imageUrl}
-              alt={clothing.type}
-              width={96}
-              height={96}
-              layout="fixed"
-            />
+          <div
+            key={index}
+            className="relative cursor-pointer group shadow-md"
+            onClick={() => toggleDetail(clothing)}
+          >
+            <div className="w-full h-0 pt-[100%] relative rounded-md border border-gray-200 overflow-hidden">
+              <Image
+                src={clothing.imageUrl}
+                alt={clothing.type}
+                layout="fill"
+                objectFit="cover"
+                className="w-full h-full"
+              />
+            </div>
           </div>
         ))}
       </div>
       {selectedClothing && (
-        <ClothingCard
-          clothing={selectedClothing}
-          onClose={closeDetail}
-          onDelete={() => handleDelete(selectedClothing.id)}
-          onSendTo={() => handleSendToWardrobe(selectedClothing)}
-          onUpdate={() => handleUpdate(selectedClothing.id)}
-          sendToLabel='Send to Wardrobe'
-        />
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+         
+            <ClothingCard
+              clothing={selectedClothing}
+              onClose={closeDetail}
+              onDelete={() => handleDelete(selectedClothing.id)}
+              onSendTo={() => handleSendToWardrobe(selectedClothing)}
+              onUpdate={() => handleUpdate(selectedClothing.id)}
+              sendToLabel='Send to Wardrobe'
+            />
+          </div>
+       
       )}
     </div>
   );
