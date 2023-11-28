84e2499cb4d62a1b2eaadb8e0118e174aeaa627d
samimu7 <samikhishali@gmail.com>
2023-11-28 17:02:41 +0100

    refactor storage page

--- frontend/app/storage/page.tsx
+++ frontend/app/storage/page.tsx
@@ -3,12 +3,20 @@
 import React from 'react';
 
 import StorageCard from '../components/storage/StorageCard';
+import Link from 'next/link';
 
 export default function Storage() {
   return (
-    <main className="flex flex-col items-center w-full p-3">
+    <main>
+      <div className="flex flex-col items-center w-full p-3">
     <h1 className="text-2xl font-bold text-primary-content text-center">My Storage</h1>
     <StorageCard/>
+    </div>
+    <footer className="bg-neutral py-10 text-center text-sm text-base-100">
+      <Link href="https://github.com/saltify-mob/WardrobeWiz" className="text-accent text-base-100 hover:underline">
+          © 2023 Saltify™. All Rights Reserved.
+      </Link>
+    </footer>
   </main>
   );
 }
