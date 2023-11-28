6f1a2bda39d1b77d42c12658556b875a4913bca8
Grim <grim.e.kalman@gmail.com>
2023-11-28 16:07:54 +0100

    Add styling on the clothing cards

--- frontend/app/components/clothingCard/ClothingCard.tsx
+++ frontend/app/components/clothingCard/ClothingCard.tsx
@@ -29,9 +29,9 @@ const ClothingCard: React.FC<ClothingCardProps> = ({ clothing, onClose, onDelete
   }, [cardRef, onClose]);
 
   return (
-    <div className="w-full h-full flex items-center justify-center bg-white bg-opacity-90">
-      <div ref={cardRef as React.MutableRefObject<HTMLDivElement>} className="max-w-md p-4 bg-white shadow-md rounded-md">
-      <Button onClick={onDelete} className="btn btn-error mt-1 w-full mx-15 px-18 py-2 bg-error text-white font-semibold rounded-lg md:w-20 lg:w-20 md:mx-3 lg:mx-3">Delete</Button>
+    <div className="flex items-center justify-center bg-white bg-opacity-90 h-screen w-screen">
+      <div ref={cardRef as React.MutableRefObject<HTMLDivElement>} className="w-5/6 sm:w-5/6 md:max-w-md p-4 bg-white shadow-md rounded-md">
+              <Button onClick={onDelete} className="btn btn-error mt-1 w-full mx-15 px-18 py-2 bg-error text-white font-semibold rounded-lg md:w-20 lg:w-20 md:mx-3 lg:mx-3">Delete</Button>
         <Image
           src={clothing.imageUrl}
           alt={clothing.type}
@@ -40,8 +40,10 @@ const ClothingCard: React.FC<ClothingCardProps> = ({ clothing, onClose, onDelete
           height={227}
           objectFit="cover"
         />
-          <Button onClick={onSendTo} className="btn btn-secondary mt-4 w-full mx-15 px-20 py-2 bg-secondary text-white font-semibold rounded-lg md:w-20 lg:w-20 md:mx-3 lg:mx-3">{sendToLabel}</Button>
+          <div className='flex-col lg:flex-row'>
+          <Button onClick={onSendTo} className="btn btn-secondary mt-4 w-full mx-15 px-20 py-2 bg-secondary text-white font-semibold rounded-lg md:w-20 lg:w-20 md:mx-6 lg:mx-6">{sendToLabel}</Button>
           <Button onClick={onUpdate} className="btn btn-primary mt-4 w-full mx-15 px-20 py-2 bg-primary text-white font-semibold rounded-lg md:w-20 lg:w-20 md:mx-3 lg:mx-3">Update</Button>
+          </div>
       </div>
     </div>
   );
