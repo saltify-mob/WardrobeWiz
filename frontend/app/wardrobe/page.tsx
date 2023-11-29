da66f38fb8cd8de63579d4b0d47b73278d86636c
Grim <grim.e.kalman@gmail.com>
2023-11-29 19:50:46 +0100

    Refactor the wardrobe page

--- frontend/app/wardrobe/page.tsx
+++ frontend/app/wardrobe/page.tsx
@@ -19,7 +19,7 @@ export default function Wardrobe() {
     const storedWeatherData = localStorage.getItem('weatherData');
     if (storedWeatherData) {
       const weatherData = JSON.parse(storedWeatherData);
-  
+
       if (weatherData && weatherData.main) {
         const currentTemp = weatherData.main.temp;
         const season = determineSeason(currentTemp);
@@ -36,17 +36,17 @@ export default function Wardrobe() {
     const currentMonth = new Date().getMonth();
 
     if (temp >= 25) {
-        return "summer";
+      return "summer";
     } else if (temp >= 10) {
-        if (currentMonth >= 0 && currentMonth <= 5) {
-            return "spring";
-        } else {
-            return "autumn";
-        }
+      if (currentMonth >= 0 && currentMonth <= 5) {
+        return "spring";
+      } else {
+        return "autumn";
+      }
     } else {
-        return "winter";
+      return "winter";
     }
-}
+  }
 
   const handleItemClick = (item: ClothingItem) => {
     setSelectedClothing(item);
@@ -86,24 +86,6 @@ export default function Wardrobe() {
 
   return (
     <main className="flex flex-col items-center w-full p-10">
-       <div className="p-4 bg-white shadow bg-white rounded-xl shadow-lg mt-4 w-full md:w-3/4 mx-auto">
-    <h1 className="text-xl font-semibold">Consider storing these clothes</h1>
-    {clothesToSendToStorage.length > 0 ? (
-      <div className='grid grid-cols-2 sm:grid-cols-4 gap-3'>
-        {clothesToSendToStorage.map(item => (
-          <div key={item.id} className='card bg-gray-100 rounded-lg p-2 shadow hover:shadow-md transition duration-300 cursor-pointer' onClick={() => handleItemClick(item)}>
-            <img src={item.imageUrl} alt={`${item.type}`} className="w-full h-20 object-cover rounded-md" />
-            <div className="text-center mt-1">
-              <h3 className="text-sm">{item.color} {item.type}</h3>
-              <p className="text-xs text-gray-600">{item.season}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    ) : (
-      <p>All out-of-season items are already in storage.</p>
-    )}
-  </div>
       {
         selectedClothing && (
           <div className="fixed inset-0 flex items-center justify-center z-50">
@@ -119,6 +101,24 @@ export default function Wardrobe() {
         )
       }
       <WardrobeCard />
+      <div className="p-4 bg-white shadow bg-white rounded-xl shadow-lg mt-4 w-full md:w-3/4 mx-auto">
+        <h1 className="text-xl font-semibold">Consider storing these clothes</h1>
+        {clothesToSendToStorage.length > 0 ? (
+          <div className='grid grid-cols-2 sm:grid-cols-4 gap-3'>
+            {clothesToSendToStorage.map(item => (
+              <div key={item.id} className='card bg-gray-100 rounded-lg p-2 shadow hover:shadow-md transition duration-300 cursor-pointer' onClick={() => handleItemClick(item)}>
+                <img src={item.imageUrl} alt={`${item.type}`} className="w-full h-20 object-cover rounded-md" />
+                <div className="text-center mt-1">
+                  <h3 className="text-sm">{item.color} {item.type}</h3>
+                  <p className="text-xs text-gray-600">{item.season}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>All out-of-season items are already in storage.</p>
+        )}
+      </div>
     </main>
   );
 }
