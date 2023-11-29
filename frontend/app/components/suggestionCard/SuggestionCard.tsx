0200af7241c5e6a93cff3b116988963ac8ae5c0a
JacobLars <jacoblar2001@gmail.com>
2023-11-29 17:57:56 +0100

    add: message displayed when wardrobe is empty

--- frontend/app/components/suggestionCard/SuggestionCard.tsx
+++ frontend/app/components/suggestionCard/SuggestionCard.tsx
@@ -8,15 +8,20 @@ const SuggestionCard = () => {
     topUrl: 'https://via.placeholder.com/150?text=Top',
     trousersUrl: 'https://via.placeholder.com/120x180?text=Trousers',
   });
-
+  
+  const [isEmptyMessage, setIsEmptyMessage] = useState<string>("Today's Outfit");
+  const [isEmpty, setIsEmpty] = useState<boolean>(false);
   const loadNewOutfit = async () => {
     try {
       const outfitData = await fetchOutfitData();
       console.log('Fetched Outfit Data:', outfitData);
       setOutfit(outfitData);
+      
     } catch (error) {
-      console.error('Error:', error);
+      setIsEmptyMessage('Looks like your wardrobe is empty! Get started by adding clothes.');
+      setIsEmpty(true);
     }
+   
   };
 
   useEffect(() => {
@@ -26,7 +31,18 @@ const SuggestionCard = () => {
   return (
     <>
       <div className="w-full flex flex-col items-center rounded-l">
-        <h2 className="text-base sm:text-2xl font-semibold my-2 sm:my-4">{`Today's Outfit`}</h2>
+
+        {isEmpty ?
+        <div role="alert" className="alert alert-info w-full mb-5 my-5 md:w-1/4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+          </svg>
+            <span className='text-lg'>Looks like your wardrobe is empty! Get started by adding clothes.</span>
+        </div>
+          :
+          <h2 className="text-base sm:text-2xl font-semibold my-2 sm:my-4">Today's Outfit</h2>
+        }
+
         <div className="flex flex-col items-center mb-6 space-y-4">
           <Image
             src={outfit.headwearUrl}
