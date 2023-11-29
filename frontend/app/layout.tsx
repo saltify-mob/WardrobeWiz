35c92e0b010694b4b49229b1e24121d10091ff80
AnneMariel95 <anne-mariel.catapang@appliedtechnology.se>
2023-11-29 12:24:01 +0100

    update footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { UserProvider } from '@auth0/nextjs-auth0/client';
 import './globals.css';
 import { WardrobeProvider } from './contexts/wardrobeContext';
 import HamburgerMenu from './components/hamburgerMenu/HamburgerMenu';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'Wardrobe Wiz',
@@ -19,11 +20,18 @@ export default function RootLayout({
     <html lang="en" data-theme="nord">
       <UserProvider>
         <WardrobeProvider>
-          <body
-            className="h-screen w-screen"
-          >
+          <body className="h-screen w-screen flex flex-col">
             <HamburgerMenu />
-            {children}
+            <div className="flex-1  pb-4">{children}</div>
+
+            <footer className="bg-neutral py-5 mt-auto text-center text-sm text-base-100">
+              <Link
+                href="https://github.com/saltify-mob/WardrobeWiz"
+                className="text-base-100 hover:underline"
+              >
+                © 2023 Saltify™. All Rights Reserved.
+              </Link>
+            </footer>
           </body>
         </WardrobeProvider>
       </UserProvider>
--- frontend/app/storage/page.tsx
+++ frontend/app/storage/page.tsx
@@ -9,14 +9,8 @@ export default function Storage() {
   return (
     <main>
       <div className="flex flex-col items-center w-full p-3">
-
-    <StorageCard/>
-    </div>
-    <footer className="bg-neutral py-10 text-center text-sm text-base-100">
-      <Link href="https://github.com/saltify-mob/WardrobeWiz" className="text-accent text-base-100 hover:underline">
-          © 2023 Saltify™. All Rights Reserved.
-      </Link>
-    </footer>
-  </main>
+        <StorageCard />
+      </div>
+    </main>
   );
 }
